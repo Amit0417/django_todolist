5ab3da24343199e5bd7630313f18d9ec95ab8ec4
AMIT KUMAR MAHOUR <r.amit.mahour@gmail.com>
2022-02-15 16:03:14 +0530

    new update

--- todolist/base/views.py
+++ todolist/base/views.py
@@ -1,7 +1,10 @@
+from dataclasses import field, fields
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView ,UpdateView ,DeleteView
+from django.urls import reverse_lazy
 from . models import Task
 
 class TaskList(ListView):
@@ -13,3 +16,19 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+
